9c806900c1d3d950b95a09f59b0ce5b38be01003
MercyAkpan <akpanmeme@gmail.com>
2023-10-05 05:05:24 -0700

    alx

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main -This is the main fn of the C Program
+* Return: Alwayts 0 for success
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
