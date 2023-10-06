6ed3e53707e8ca6c88bc9b449f5cdfe075f12eff
MercyAkpan <akpanmeme@gmail.com>
2023-10-06 10:13:08 -0700

    Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.
    You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
    All your code should be in the main function
    You can only use putchar three times in your code

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main -ENTRY POINT
+  * Return: 0 on SUCCESS
+  */
+int main(void)
+{
+	int a, b;
+
+	for (a = 48; a <= 57; a++)
+	{
+	putchar(a);
+	}
+	for (b = 97; a <= 102; a++)
+	{
+	putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
