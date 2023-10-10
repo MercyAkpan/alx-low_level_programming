30c0ac1b1ba25cd495862e828ca06cdac694b557
MercyAkpan <akpanmeme@gmail.com>
2023-10-10 15:35:04 -0700

    alx

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print nunber
+ * @n: variable
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+		if (n == 98)
+		{
+		printf("%d\n", n);
+		break;
+		}
+		printf("%d, ", n);
+		}
+	}
+	else if (n >= 98)
+	{
+		for (; n > 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d\n", n);
+				break;
+			}
+			printf("%d, ", n);
+		}
+	}
+}
