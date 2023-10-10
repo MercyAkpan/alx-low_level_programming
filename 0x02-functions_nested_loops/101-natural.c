3d3e2f309f298eef74381d0a4e131965f791b08a
MercyAkpan <akpanmeme@gmail.com>
2023-10-10 13:38:28 -0700

    alx

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int a, sum = 0;
+
+	a = 0;
+	while (a < 1024)
+	{
+	if ((a % 3 == 0) || (a % 5 == 0))
+		{
+			 sum = 0;
+			 sum = sum + a;
+		}
+		a++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
