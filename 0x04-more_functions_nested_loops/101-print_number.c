41f4450100a46a060dfc93c4c74b5a092b361b8d
MercyAkpan <akpanmeme@gmail.com>
2023-10-12 14:50:06 -0700

    alx

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * print_number - this prints numbers
+  * @n: variables
+  */
+void print_number(int n)
+{
+	int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	};
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
+
