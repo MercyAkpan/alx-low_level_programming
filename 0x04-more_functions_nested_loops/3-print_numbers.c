07014b75ae56b1673c267e4bb410ebd9f4757d43
MercyAkpan <akpanmeme@gmail.com>
2023-10-12 08:52:48 -0700

    Write a function that prints the numbers, from 0 to 9, followed by a new line.
    Prototype: void print_numbers(void);
    You can only use _putchar twice in your code

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * print_numbers - this prints required numbers
+  */
+void print_numbers(void)
+{
+	int d;
+
+	for (d = 48; d <= 57; d++)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+}
