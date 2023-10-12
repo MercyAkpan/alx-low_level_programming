618c6b833efddd957ff4269307c23af0b34c92df
MercyAkpan <akpanmeme@gmail.com>
2023-10-12 12:07:09 -0700

    alx

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,29 +5,31 @@
   */
 int main(void)
 {
-	int a = 1, h, j;
+	int a = 1;
 
 	for (; a <= 100; a++)
 	{
-		h = a / 10;
-		j = a % 10;
 		if ((a % 3 == 0) && (a % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (a % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (a % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
-		else if (a > 9)
+		else
 		{
-			printf("%d", h);
+			printf("%d ", a);
 		}
-		printf("%d ", j);
+		if (a < 100)
+		{
+			printf(" ");
+		}
 	}
+	printf("\n");
 	return (0);
 	}
