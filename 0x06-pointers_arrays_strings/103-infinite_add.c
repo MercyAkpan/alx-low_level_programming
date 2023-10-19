17d8f997ea2af464401e5765993dd3678d61284c
MercyAkpan <akpanmeme@gmail.com>
2023-10-19 16:53:14 -0700

    alx

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * infinite_add - This is the function
+  * @n1: this variable
+  * @n2: 2nd varia ble
+  *@r: this is result
+  *@size_r:  this  is buffer size
+  * Return: this returns results
+  */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (r);
+}
