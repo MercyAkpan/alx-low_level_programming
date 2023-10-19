0354055e24fd2ca63c39021cc53f54e8e6620b08
MercyAkpan <akpanmeme@gmail.com>
2023-10-19 12:57:35 -0700

    Write a function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
-  *
-  *
-  *
+  * print_number - this prints required number
+  * @n: this is the number to be printed
   */
 void print_number(int n)
 {
-	while (n >= 48 && n <= 57)
-	{
 		if (n < 0)
 		{
 			_putchar ('-');
+			n = -n;
 		}
-		if (n > 9)
+		if (n > 999)
 		{
-			_putchar ((n / 10) + '0');
-			_putchar((n % 10 ) + '0');
+			_putchar((n / 1000) + '0');
+			_putchar((n / 100) % 10 + '0');
+			_putchar((n / 10) % 10 + '0');
+			_putchar((n % 10) + '0');
 		}
 		else if (n > 99)
 		{
@@ -23,16 +23,13 @@ void print_number(int n)
 			_putchar((n / 10) % 10 + '0');
 			_putchar((n % 10) + '0');
 		}
-		else if(n > 999)
+		else if (n > 9)
 		{
-			_putchar((n / 1000) + '0');
-			_putchar((n / 100) % 10 + '0');
-			_putchar((n / 10) % 10 + '0');
-			_putchar((n % 10 ) + '0');
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
 		}
 		else
 		{
 			_putchar(n + '0');
 		}
-	}
 }
