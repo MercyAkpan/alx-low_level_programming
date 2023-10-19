9abd57920471a02cd0e39f8f78ddc9428233f82f
MercyAkpan <akpanmeme@gmail.com>
2023-10-19 15:03:38 -0700

    print integers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,36 +5,40 @@
   */
 void print_number(int n)
 {
+		unsigned int x;
+		x = n;
+
 		if (n < 0)
 		{
 			_putchar ('-');
-			n = -n;
+			x = -n;
 		}
-		if (n / 10 != 0)
+		if (x / 10 != 0)
 		{
-			print_number(n / 10);
+			print_number(x / 10);
 		}
-		_putchar((n % 10) + '0');
+		_putchar((x % 10) + '0');
 		/**if (n > 999)
-		{
-			_putchar((n / 1000) + '0');
-			_putchar((n / 100) % 10 + '0');
-			_putchar((n / 10) % 10 + '0');
-			_putchar((n % 10) + '0');
-		}
-		else if (n > 99)
-		{
-			_putchar((n / 100) + '0');
-			_putchar((n / 10) % 10 + '0');
-			_putchar((n % 10) + '0');
-		}
-		else if (n > 9)
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-		}
-		else
-		{
-			_putchar(n + '0');
-		}*/
+		*{
+			*_putchar((n / 1000) + '0');
+			*_putchar((n / 100) % 10 + '0');
+			*_putchar((n / 10) % 10 + '0');
+			*_putchar((n % 10) + '0');
+		*}
+		*else if (n > 99)
+		*{
+			*_putchar((n / 100) + '0');
+			*_putchar((n / 10) % 10 + '0');
+			*_putchar((n % 10) + '0');
+		*}
+		*else if (n > 9)
+		*{
+			*_putchar((n / 10) + '0');
+			*_putchar((n % 10) + '0');
+		*}
+		*else
+		*{
+			*_putchar(n + '0');
+		*}
+		 */
 }
