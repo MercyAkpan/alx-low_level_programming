4c2b1037f84a0a7fd9105210f4b610f47073c2f1
MercyAkpan <akpanmeme@gmail.com>
2023-10-21 15:23:38 +0100

    alx

--- 104-print_buffer.c
+++ 104-print_buffer.c
@@ -11,7 +11,22 @@ void print_buffer(char *b, int size)
 	
 	while (bi < size)
 	{
-
+		printf ("%08x:", b[bi]);
+		j = 0;
+		while ((j < 10 && u % 10 != 0) || u = 0)
+		{
+			u = bi + j;
+			printf (" %02x", b[u]);
+			/*j++;*/
+			u += 1;
+			printf ("%02x", b[u]); 
+			j = j + 2;/* can I add j++ here, and make it a while loop*/
+		}
+		printf {" "};
+		/*charcter code*/
+		prinf("\n");
+		bi = bi + 10;
 	}
+
 
 }
