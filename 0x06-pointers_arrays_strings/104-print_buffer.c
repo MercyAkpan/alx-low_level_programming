65c3beb226d9816d248bbf6f6ea814f87d8e96d2
MercyAkpan <akpanmeme@gmail.com>
2023-10-21 18:44:39 +0100

    alx

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,7 +1,63 @@
 #include "main.h"
+#include <stdio.h>
 /**
+  *
+  *
   *
   *
   */
 void print_buffer(char *b, int size)
-{}
+{
+	int bi = 0, j = 0, u = 0, x, h;
+	
+	while (bi < size)
+	{
+		printf ("%08x:", bi);
+		j = 0;
+		if (size - bi < 10)
+		{
+			h = size - bi;
+		}
+		else 
+			h = 10;
+		while ((j < 10 && u % 10 != 0) || u == 0)
+		{
+			if (j < h)
+			{
+			u = bi + j;
+			printf (" %02x", b[u]);
+			/*j++;*/
+			u += 1;
+			printf ("%02x", b[u]); 
+			j = j + 2;/* can I add j++ here, and make it a while loop*/
+			}
+			else
+			{
+				j = 10 - h;
+				while (j > 0)
+				{
+					printf("     ");
+					j = j - 2;
+				}
+				break;
+			}
+		}
+		printf(" ");
+		for (j = 0; j < h; j++)
+		{
+			x = *(b + bi + j);
+			if (x >= 32 && x <= 132)
+			{
+			printf ("%c", x);
+			}
+			else
+			{
+				printf(".");
+			}
+		}
+		printf("\n");
+		bi = bi + 10;
+	}
+
+
+}
