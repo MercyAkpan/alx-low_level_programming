9d0208cf86c1ddb7d29a6db75e1f09817d23e79c
MercyAkpan <akpanmeme@gmail.com>
2023-10-23 23:22:13 +0100

    alx

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  *
+  *
+  *
+  *
+  */
+unsigned int _strspn(char *s, char *accept)
+/*NB:Return value is not a pointer/array so R-Value can be just a number*/
+{
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _strpbrk - this returns first occurence of a pointer
+  * @s: this is the pointer to be assessed
+  * @accept: this is the 2nd pointer to compare with
+  * Return: This returns the first occurnce or NULL.
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				for (; s[i] != '\0'; i++)
+				{
+					printf("%c", s[i]);
+				}
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
