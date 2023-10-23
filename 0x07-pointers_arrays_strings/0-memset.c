e2ffa01831d945956a52490769fff79dafa399a1
MercyAkpan <akpanmeme@gmail.com>
2023-10-23 21:54:50 +0100

    Write a function that fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _memset - this sets an array element to a constant
+  * @s: this is the array to be checked
+  * @b: this is the constant
+  * @n: this is the limited number of times to modify the array elements
+  *Return: this points to the now modified array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int e;
+	int y = 0;
+
+	for (e = 1; e <= n; e++)
+	{
+		s[y] = b;
+		printf("%c", s[y]);
+		y++;
+	}
+	return (s);
+}
