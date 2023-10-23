848686cccf50d1314a2f7bed9d7683cc9bd7fd00
MercyAkpan <akpanmeme@gmail.com>
2023-10-23 22:27:43 +0100

    Write a function that locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+  * _strchr - this returns from 1st occurenec of a letter
+  * @s: this is the pointer to be assessed.
+  * @c: this is the character to be matched
+  * Return: this returns the 1st occurence onwards or NULL.
+  */
+char *_strchr(char *s, char c)
+{
+	unsigned int kk;
+
+	for (kk = 0; s[kk] != '\0'; kk++)
+	{
+		if (s[kk] == c)
+		{
+			for (; s[kk] != '\0'; kk++)
+			{
+				printf("%c", s[kk]);
+			}
+		}
+		else if (s[kk + 1] == '\0')
+			return (NULL);
+	}
+	printf("\n");
+	return (0);
+}
