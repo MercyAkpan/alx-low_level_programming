7e39a3da73a509a27cdf6558ca874373e0467414
MercyAkpan <akpanmeme@gmail.com>
2023-10-31 09:02:32 -0700

    Write a function that creates an array of chars, and initializes it with a specific char.
    Prototype: char *create_array(unsigned int size, char c);

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * create_array - this is used to init every xter in an array to a given xter
+  * @size: this is the max amount of bytes allocated
+  * @c: this is the specified xter
+  * Return: this returns the pointer to the array
+  */
+char *create_array(unsigned int size, char c)
+{
+	char *ptt;
+	unsigned int i;
+
+	ptt = malloc(sizeof(ptt) * size);
+	if (size == 0)
+		return (NULL);
+	if (ptt == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ptt[i] = c;
+	return (ptt);
+	free(ptt);
+}
