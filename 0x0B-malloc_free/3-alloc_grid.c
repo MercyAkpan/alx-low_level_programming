669890a5ae328f62f9cbcfb67693795787609878
MercyAkpan <akpanmeme@gmail.com>
2023-11-02 00:00:42 +0100

    alx

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,26 +2,30 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
-  *
-  *
-  *
-  *
-  *
-  */
+  * alloc_grid - This create an array of pointers to integers
+  * @width: this is the column-size of the array of arrays
+  * @height: This is the row-size of the  array of arrays
+ * Return: This returns an integer value - The jth value
+ */
 int **alloc_grid(int width, int height)
 {
-	int *ppt, i, j, w = width, h = height;
-	
+	int **ppt, i, j, w = width, h = height;
+
 	if (w <= 0 || h <= 0)
 		return (NULL);
-	ppt = (int *)malloc(sizeof(int) * (w * h));
+	ppt = malloc(sizeof(int *) * h);
 	if (ppt == NULL)
 		return (NULL);
 	for (i = 0; i < w; i++)
 	{
-		for (j = 0; j < h; j++)
-			*(*ppt)[i][j] = 0;
+		ppt[i] = malloc(sizeof(int) * w);
+		if (ppt[i] == NULL)
+			/*for (; i >= 0; i--)*/
+			return (NULL);
 	}
-	return (*ppt);
+	for (i = 0; i < h; i++)
+		for (j = 0; j < w; j++)
+			ppt[i][j] = 0;
+	return (ppt);
 	free(ppt);
 }
