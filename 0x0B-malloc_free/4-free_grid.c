77580011454d530bf4fcb8fd176f4e88ebf5b409
MercyAkpan <akpanmeme@gmail.com>
2023-11-02 03:08:54 +0100

    alx

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -7,9 +7,10 @@
   */
 void free_grid(int **grid, int height)
 {
-	if (grid == NULL || height <= 0)
-		return;
+	if (grid != NULL && height > 0)
+	{
 	for (height--; height >= 0; height--)
 		free(grid[height]);
+	}
 	free(grid);
 }
