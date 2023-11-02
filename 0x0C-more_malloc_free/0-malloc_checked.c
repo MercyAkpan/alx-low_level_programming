0357811c7d1ea319a2dd0c0d8113d7f94ca72eab
MercyAkpan <akpanmeme@gmail.com>
2023-11-03 00:46:56 +0100

    alx

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-  *
-  *
-  *
-  *
-  *
+  * malloc_checked - This checks a malloc
+  * Return: This returns an exit code or pointer to memory allocated.
+  * @b: This is the sample of mem allocated.
   */
 void *malloc_checked(unsigned int b)
 {
-	unsigned int *xtr;
-	
+	void *xtr;
+
 	xtr = malloc(b);
 	if (xtr == NULL)
-		exit (98);
+		exit(98);
 	return (xtr);
 }
