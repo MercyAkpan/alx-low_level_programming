fe6028efa4cd39dd74cdaaa4eb1dec87a655e4b1
MercyAkpan <akpanmeme@gmail.com>
2023-11-02 03:06:01 +0100

    alx

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * free_grid - This frees a pre-mentioned grid
+  * @grid: Thisnis nthe specified grid to be freed(an array of pointers).
+  * @height: This is the height(row number) of the grid.
+  */
+void free_grid(int **grid, int height)
+{
+	if (grid == NULL || height <= 0)
+		return;
+	for (height--; height >= 0; height--)
+		free(grid[height]);
+	free(grid);
+}
