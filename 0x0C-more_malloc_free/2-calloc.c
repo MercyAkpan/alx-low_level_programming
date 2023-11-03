004179fe9a8af90a3efcd6f475aec7cd910ab880
MercyAkpan <akpanmeme@gmail.com>
2023-11-03 01:34:33 +0100

    alx

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,18 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-  *
-  *
-  *
-  *
-  */
+  * _calloc - This creates memory for an array
+  * @nmemb: This is the integral memeory spaces eg 10,34,98.
+  * @size: This is sizeof, argument eg sizeof(char),(int)...
+ * Return: THis returns a pointer to memeory allocated for an array.
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int * xxb;
-	
+	unsigned int *xxb;
+
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	* xxb = malloc(size * nmemb);
+	xxb = malloc(size * nmemb);
 	if (xxb == NULL)
 		return (NULL);
 	return (xxb);
