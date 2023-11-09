b67ad97ba60becfbcda03508fd74fe5f7649e444
MercyAkpan <akpanmeme@gmail.com>
2023-11-09 22:48:40 +0100

    alx

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef "dog.h"
+#define "dog.h"
+#ifndef "main_h"
+#define "main_h"
+/**
+  * struct dog - this is the struct of type "dog"
+  * @name: this is the variable
+  * @age: this is the 2nd variable
+  * @owner: This is the 3rd varible
+  * Description: This is the desription.
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
