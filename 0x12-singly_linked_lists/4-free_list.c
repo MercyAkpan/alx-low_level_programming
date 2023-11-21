ea6190bb29b97a8a6378e8298ca159d9c47c1569
MercyAkpan <akpanmeme@gmail.com>
2023-11-21 14:07:31 -0800

    alx

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+  * free_list - This function frees allocated memory
+  * @head: This is the pointer to head of the linked list
+  * Return: This returns nothing
+  */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (!head)
+		return; 
+	while(head)
+	{
+		temp = head->next;;
+		free(head);
+		head = temp;
+	}
+	head = NULL;
+	free(NULL);
+}
