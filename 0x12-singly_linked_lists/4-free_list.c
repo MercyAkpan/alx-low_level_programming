14633d370510c6f952d75b010714b9ac360ed6a3
MercyAkpan <akpanmeme@gmail.com>
2023-11-21 14:18:40 -0800

    alx

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,10 +10,10 @@ void free_list(list_t *head)
 	list_t *temp;
 
 	if (!head)
-		return; 
-	while(head)
+		return;
+	while (head)
 	{
-		temp = head->next;;
+		temp = head->next;
 		free(head);
 		head = temp;
 	}
