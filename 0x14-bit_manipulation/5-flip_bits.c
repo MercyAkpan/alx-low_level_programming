d44e380900ebaceb5f4cc035f7c5f46c09a7d392
MercyAkpan <akpanmeme@gmail.com>
2023-12-13 07:48:22 +0100

    alx

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * flip_bits - This flip bits
+  * @n: This is the nth.
+  * @m: This
+  * Return: This returns a size_t integer.
+  *
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	size_t output, counter;
+
+	output = n ^ m
+	for (counter = 0; output > 0;)
+	{
+		if ((output & 1) == 1)
+			++counter;
+		output = output >> 1;
+	}
+	return (counter);
+}
+
