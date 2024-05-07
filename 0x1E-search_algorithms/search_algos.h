35383878e94d62b008815981711b45ee58360ab5
MercyAkpan <akpanmeme@gmail.com>
2024-05-07 23:42:00 +0100

    alx

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int real_binary_search(int *array, int value, int low, int high);
+void print_array(int *array, int low, int high);
 #endif
