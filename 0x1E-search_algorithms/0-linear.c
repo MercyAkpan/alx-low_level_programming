5f6e0faa11569937b6c3199764edfa690113ddbb
MercyAkpan <akpanmeme@gmail.com>
2024-05-07 12:45:41 +0100

    alx

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+/**
+	* linear_search - This uses the linear search algo,
+	*                to search for a value sequentially
+	* @array: This is a pointer to an array
+	* @size: Length of numbers in the array
+	* @value: This is the value to be searched for
+	* Return: Returns index of value (1st occurence) or -1, if not found
+	*/
+int linear_search(int *array, size_t size, int value)
+{
+/*
+	* These local variables are AUXILIARY SPACES
+	* check -- https://www.geeksforgeeks.org/g-fact-86/
+ */
+	if (array != NULL)
+	{
+		int temp, n = size;
+
+		for (temp = 0; temp < n; temp++)
+		{
+			printf("Value checked array[%d] = [%d]\n", temp, array[temp]);
+			if (array[temp] == value)
+				return (temp);
+		}
+	}
+return (-1);
+}
